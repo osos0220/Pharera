16b759e2d1e3a09ac853801deb8cf927b4da08d3
Mohamed <42010654.osos@gmail.com>
2024-06-17 17:17:06 +0300

    iOS update 17/6

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,29 @@ import 'package:Pharera/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'firebase_options.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyBiaAhRyi3LRfsgq8t_-KxtLjJaolyt9K4",
-        appId: "1:337345392254:android:c98882ec00fe4406c2722f",
-        messagingSenderId: '...',
-        projectId: "signup-login-93587",
-        // Your web Firebase config options
-      ),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // if (kIsWeb) {
+  //   await Firebase.initializeApp(
+  //     options: const FirebaseOptions(
+  //       apiKey: "AIzaSyBiaAhRyi3LRfsgq8t_-KxtLjJaolyt9K4",
+  //       appId: "1:337345392254:android:c98882ec00fe4406c2722f",
+  //       messagingSenderId: '...',
+  //       projectId: "signup-login-93587",
+  //       // Your web Firebase config options
+  //     ),
+  //   );
+  // } else {
+  //   await Firebase.initializeApp();
+  // }
   await FlutterDownloader.initialize(
       debug: false // Optional: Set false to disable printing logs to console
   );
